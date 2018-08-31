17e59aa4029d606d1e873c6a0cd2b981437901bf
Simon Legner <Simon.Legner@gmail.com>
2018-08-31 18:53:13 +0200

    build: also generate minified version

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,23 @@
+import buble from 'rollup-plugin-buble';
 import common from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import {uglify} from 'rollup-plugin-uglify';
 
-export default {
+const shared = Object.freeze({
   input: './index',
-  plugins: [nodeResolve(), common()],
+  plugins: [nodeResolve(), common(), buble()],
   output: {
     file: 'build/wikimedia-commons-file-path.js',
     format: 'umd',
     name: 'wikimedia-commons-file-path'
   }
-};
+});
+
+export default [
+  shared,
+  {
+    ...shared,
+    plugins: [nodeResolve(), common(), buble(), uglify()],
+    output: {...shared.output, file: 'build/wikimedia-commons-file-path.min.js'}
+  }
+];
