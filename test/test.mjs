693bfcf0d3a49274cd8dce928ee1e8f5abfeb51e
Simon Legner <Simon.Legner@gmail.com>
2026-06-01 22:28:43 +0200

    Respect wgThumbnailSteps
    https://www.mediawiki.org/wiki/Manual:$wgThumbnailSteps
    
    https://www.mediawiki.org/wiki/Common_thumbnail_sizes

--- test/test.mjs
+++ test/test.mjs
@@ -17,20 +17,34 @@ describe("wikimedia-commons-file-path", () => {
   });
   it("should compute a thumbnail file path", () => {
     assert.equal(
+      getFilePath("File:Innsbruck.jpg", 500),
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Innsbruck.jpg/500px-Innsbruck.jpg",
+    );
+  });
+  it("should compute a thumbnail file path matching wgThumbnailSteps", () => {
+    assert.equal(
       getFilePath("File:Innsbruck.jpg", 360),
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Innsbruck.jpg/360px-Innsbruck.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Innsbruck.jpg/500px-Innsbruck.jpg",
+    );
+    assert.equal(
+      getFilePath("File:Innsbruck.jpg", 640),
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Innsbruck.jpg/960px-Innsbruck.jpg",
+    );
+    assert.equal(
+      getFilePath("File:Innsbruck.jpg", 5500),
+      "https://upload.wikimedia.org/wikipedia/commons/2/2a/Innsbruck.jpg",
     );
   });
   it("should compute a PNG thumbnail file path for SVG file", () => {
     assert.equal(
-      getFilePath("File:Commons-logo.svg", 360),
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Commons-logo.svg/360px-Commons-logo.svg.png",
+      getFilePath("File:Commons-logo.svg", 500),
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Commons-logo.svg/500px-Commons-logo.svg.png",
     );
   });
   it("should compute a JPG thumbnail file path for TIF file", () => {
     assert.equal(
-      getFilePath("File:BassersdorffSwissair-19780725i.tif", 800),
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/BassersdorffSwissair-19780725i.tif/800px-BassersdorffSwissair-19780725i.tif.jpg",
+      getFilePath("File:BassersdorffSwissair-19780725i.tif", 960),
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/BassersdorffSwissair-19780725i.tif/960px-BassersdorffSwissair-19780725i.tif.jpg",
     );
   });
 });
